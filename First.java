0be4af378ec961ed66f7909c4ab1057c98dc3147
Branden Hernandez <114962539+BrandenHernandez@users.noreply.github.com>
2023-05-10 13:48:26 -0500

    Java application example
    Source: Java Programming 7th ed. by Joyce Farrell

---
+++ First.java
@@ -0,0 +1,13 @@
+/*
+* Author: Branden Hernandez
+* Date: 5/10/23
+* Purpose: First example in Java Programming 7th ed. by Joyce Farrell
+* */
+public class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("First Java application!");
+    }
+}
+//Thanks for looking
