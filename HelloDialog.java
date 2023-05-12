7a8b0bd26dcedae6599161b2f0082e5b206b2cfc
Branden Hernandez <114962539+BrandenHernandez@users.noreply.github.com>
2023-05-12 18:39:22 -0500

    Hello World GUI
    An example of a GUI interface using Hello World from Java Programming 7th ed. by Joyce Farrell

---
+++ HelloDialog.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Branden Hernandez
+ * Date: 5/12/23
+ * Purpose: Example of GUI in Java Programming 7th ed. by Joyce Farrell
+ * */
+import javax.swing.JOptionPane;
+public class HelloDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
+//Thanks for looking
