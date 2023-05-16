da7b1f6c0ee2e1d545db44490c761409288a4dc3
Branden Hernandez <114962539+BrandenHernandez@users.noreply.github.com>
2023-05-16 07:52:43 -0500

    Data demo example
    Data demo in Java Programming 7th ed. by Joyce Farrell

---
+++ DataDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Branden Hernandez
+ * Date: 5/16/23
+ * Purpose: Data display example in Java Programming 7th ed. by Joyce Farrell
+ * */
+public class DataDemo {
+    public static void main(String[] args){
+        int aWhoNumber = 315;
+        final int STATES_IN_US = 50;
+
+        System.out.print("The number is ");
+        System.out.println(aWhoNumber);
+        System.out.println("The number of states is " + STATES_IN_US);
+    }
+}
+//Thanks for looking
