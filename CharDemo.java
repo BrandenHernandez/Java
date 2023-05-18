7c1a40c67449830a9c3bed38610d8bbda266ff34
Branden Hernandez <114962539+BrandenHernandez@users.noreply.github.com>
2023-05-18 11:45:18 -0500

    Char Demo
    Char example in Java Programming 7th ed. by Joyce Farrell

---
+++ CharDemo.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Branden Hernandez
+ * Date: 5/18/23
+ * Purpose: Char example in Java Programming 7th ed. by Joyce Farrell
+ * */
+public class CharDemo {
+    public static void main(String[] args){
+        char initial = 'A';
+
+        System.out.println(initial);
+        System.out.println("\t\"abc\\def\bghi\n\njkl");
+    }
+}
+//Thanks for looking
